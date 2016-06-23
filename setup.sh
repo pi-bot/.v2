c19c232114e99ec8d03249a812d98397afe60851
Harry Gee <code@pibot.org>
2016-06-23 16:33:53 +0100

    Updated setup.sy

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@
 # this is a bash file intended to be run to install all the dependecies for the PiBot Code base
 # TBC
 echo "export PYTHONPATH=$PYTHONPATH:~/v2/python-code" >> ~/.profile
+# then update bash to implement the change ins PYTHONPATH
 source ~/.profile
+#moving a file to work the mission module
 mv /home/pi/v2/misc/sample_data.json /home/pi/v2/python-code/sample_data.json
