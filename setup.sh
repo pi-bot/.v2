4662548506ca5161482740524efe2374e5731e1a
Harry Gee <code@pibot.org>
2016-06-23 16:14:44 +0100

    Updated setup.sy
    Changing PYTHON path to locate the pibot python package modules.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
 # this is a bash file intended to be run to install all the dependecies for the PiBot Code base
 # TBC
+echo "export PYTHONPATH=$PYTHONPATH:~/v2/python-code" >> ~/.profile
+source ~/.profile
