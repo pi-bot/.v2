741a23b78490fe1f36121c7633dedbb7fb242a2a
Harry Gee <code@pibot.org>
2016-06-29 18:43:50 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,14 +4,18 @@
 
 # Notes for you to get code working 
 # On command line in robot update the Python path:
-  
-echo "export PYTHONPATH=$PYTHONPATH:~/v2/python-code" >> ~/.profile
-# then update bash to implement the change ins PYTHONPATH
-source ~/.profile
+
+# First we clone the repo with 
+
+git clone https://github.com/pi-bot/v2
 # now we can udate the firmware
 cd /home/pi/v2/arduino-firmware/
 ./upload.sh
 #makesure the board power switch is on
+#before we test we need to tell the system where the modules are:
+echo "export PYTHONPATH=$PYTHONPATH:~/v2/python-code" >> ~/.profile
+# then update bash profile to implement the change ins PYTHONPATH
+source ~/.profile
 # Then we can test
 cd /home/pi/v2/tests/
 sudo python testAll.py
