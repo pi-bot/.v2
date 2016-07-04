6e2bd69911e2c01529393d7e5b7840ccea812dd2
Harry Gee <code@pibot.org>
2016-07-04 14:36:05 +0100

    Create td2.py
    Tests the new functions

---
+++ tests/td2.py
@@ -0,0 +1,24 @@
+from arduino import Commands, Arduino
+from time import sleep
+from motors import Motors
+from mission import Mission
+board = Arduino()
+board.connect()
+move = Motors()
+mission = Mission()
+mission.startMission()
+
+move.position(10,100)
+there=int(move.getAtPosition())
+#print(there)
+while (there==0):
+        print("waiting for desination")
+        sleep(0.2)
+        there=int(move.getAtPosition())
+
+sleep(1)
+print("Arrived there")
+move.rotate(-90,100)
+sleep(1)
+move.position(-10,100)
+~                                                                                                                                           1,1           All
