03298dc598c68a2f92a6a5abd9dbc999d6329dab
Harry Gee <code@pibot.org>
2016-07-18 15:54:34 +0100

    Update up.sh

--- arduino-firmware/up.sh
+++ arduino-firmware/up.sh
@@ -4,4 +4,6 @@
 #sudo ino clean
 #sudo ino build
 #flash the arduino witch the scketch
-sudo avrdude -p atmega328p -P /dev/ttyS0 -c arduino -b 115200 -D -U flash:w:.build/uno/firmware.hex:i -vv
+sudo avrdude -p atmega328p -P /dev/ttyAMA0 -c arduino -b 115200 -D -U flash:w:.build/uno/firmware.hex:i -vv
+# this next verion is for use with the Raspberry Pi 3. So please comment out above and delete # from below if you need to use it!
+# sudo avrdude -p atmega328p -P /dev/ttyS0 -c arduino -b 115200 -D -U flash:w:.build/uno/firmware.hex:i -vv
