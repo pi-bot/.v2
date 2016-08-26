6259e899b12251647e449686cca17d8cce2940a3
Harry Gee <code@pibot.org>
2016-08-26 10:58:51 +0100

    Update motors.py

--- python-code/motors.py
+++ python-code/motors.py
@@ -1,8 +1,7 @@
-#
 #  	New version for "PiBot v8.0" With improved motor control IC DRV8835
 #	also updated with new pin definitions for new PCB
 #
-#		by Jason 10/05/2016
+#		by Harry / Matt  10/08/2016
 #
 
 import arduino
@@ -112,7 +111,7 @@ class Motors():
         return command
 
      # lower level command to move
-    def position(self,pos,speed,block):
+    def position(self,pos,speed=DEFAULT_SPEED,block=True):
         self.board.sendCommand(Commands.POSITION,DUMMY_PIN,pos)
                 
         if block:
@@ -122,162 +121,31 @@ class Motors():
                 sleep(0.2)
             print("got there! - Now I'll do  next command")
 
-    def moveForward(self,pos,speed=DEFAULT_SPEED):
-        self.position(pos,speed)
 
-    def moveBackward(self,pos,speed=DEFAULT_SPEED):
-        self.position(-pos)
 
-    def rotate(self,angle,speed):
+    def rotate(self,angle,speed=DEFAULT_SPEED,block):
         self.board.sendCommand(Commands.ROTATE,DUMMY_PIN,angle)
 
-    def turnLeft(self,angle,speed=DEFAULT_SPEED):
-        self.rotation(speed, -angle)
-
-    def turnRight(self,angle,speed=DEFAULT_SPEED):
-        self.rotation(speed, angle)
-
-    def __del__(self):
-        self.stop()
-
-#sleep(1)
-#print board.sendCommand(Commands.READ_ULTRASOUND,17,19)
+        if block:
+            there=int(self.getAtPosition())
+            while int(self.getAtPosition()) == 0:
+                print("on my way")
+                sleep(0.2)
+            print("got there! - Now I'll do  next command")
 
 
 
+    def moveForward(self,pos,speed=DEFAULT_SPEED, block=True):
+        self.position(pos)
 
+    def moveBackward(self,pos,speed=DEFAULT_SPEED, block=True):
+        self.position(-pos)
 
+    def turnLeft(self,angle,speed=DEFAULT_SPEED, block=True):
+        self.rotate(speed, -angle)
 
-
-'''
-class Motors():
-    def __init__(self):
-        self.board = Arduino()
-        self.board.connect()
-
-    def enable(self): #arduino.A2
-        self.board.sendCommand(Commands.WRITE_DIGITAL,7,1)
-
-    def disable(self):
-        self.board.sendCommand(Commands.WRITE_DIGITAL,7,0)
-
-    def forward(self, speed):
-        self.disable()
-        command = self.speedToCommand(speed)
-        self.board.sendCommand(Commands.WRITE_PWM,LEFT_MOTOR_DIRECTION,0)
-        self.board.sendCommand(Commands.WRITE_PWM,RIGHT_MOTOR_DIRECTION,0)
-        self.board.sendCommand(Commands.WRITE_PWM,LEFT_MOTOR_SPEED,command)
-        self.board.sendCommand(Commands.WRITE_PWM,RIGHT_MOTOR_SPEED,command)
-        #commands are set, enable the motors
-        self.enable()
-
-	def getLeft(self):
-		value = self.board.sendCommand(Commands.READ_LEFT_ENCODER,0,0)
-		return value
-
-	def getRight(self):
-		value = self.board.sendCommand(Commands.READ_RIGHT_ENCODER,0,0)
-		return value
-
-    def backward(self, speed):
-        self.disable()
-        command = self.speedToCommand(speed)
-        self.board.sendCommand(Commands.WRITE_PWM,LEFT_MOTOR_SPEED,0)
-        self.board.sendCommand(Commands.WRITE_PWM,RIGHT_MOTOR_SPEED,0)
-        self.board.sendCommand(Commands.WRITE_PWM,LEFT_MOTOR_DIRECTION,command)
-        self.board.sendCommand(Commands.WRITE_PWM,RIGHT_MOTOR_DIRECTION,command)
-        #commands are set, enable the motors
-        self.enable()
-
-    def leftMotor(self, speed, direction):
-        self.disable()
-        command = self.speedToCommand(speed)
-        if(direction==1):
-            self.board.sendCommand(Commands.WRITE_PWM,LEFT_MOTOR_DIRECTION,0)
-            self.board.sendCommand(Commands.WRITE_PWM,LEFT_MOTOR_SPEED,command)
-        else:
-            self.board.sendCommand(Commands.WRITE_PWM,LEFT_MOTOR_SPEED,0)
-            self.board.sendCommand(Commands.WRITE_PWM,LEFT_MOTOR_DIRECTION,command)
-        self.enable()
-
-    def rightMotor(self, speed, direction):
-        self.disable()
-        command = self.speedToCommand(speed)
-        if(direction==1):
-            self.board.sendCommand(Commands.WRITE_PWM,RIGHT_MOTOR_DIRECTION,0)
-            self.board.sendCommand(Commands.WRITE_PWM,RIGHT_MOTOR_SPEED,command)
-        else:
-            self.board.sendCommand(Commands.WRITE_PWM,RIGHT_MOTOR_SPEED,0)
-            self.board.sendCommand(Commands.WRITE_PWM,RIGHT_MOTOR_DIRECTION,command)
-        self.enable()
-
-    def stop(self):
-        #self.disable()
-        self.board.sendCommand(Commands.WRITE_DIGITAL,LEFT_MOTOR_DIRECTION,0)
-        self.board.sendCommand(Commands.WRITE_DIGITAL,RIGHT_MOTOR_DIRECTION,0)
-        self.board.sendCommand(Commands.WRITE_DIGITAL,LEFT_MOTOR_SPEED,0)
-        self.board.sendCommand(Commands.WRITE_DIGITAL,RIGHT_MOTOR_SPEED,0)
-        self.position(0)
-        self.rotation(0)
-
-    def speedToCommand(self,speed):
-        #speed between 0-100
-        command = int(speed * 2.55)
-        if(command > 255):
-            command = 255
-        return command
-
-
-    def position(self,pos,speed)
-        speed = speedToCommand(speed)
-        self.disable()
-        self.board.sendCommand(Commands.POSITION,speed,pos)
-        self.enable()
-
-    def position(self,pos):
-        position(pos,DEFAULT_SPEED)
-
-
-    def moveForward(self,pos,speed):
-	self.position(pos,speed)
-
-    def moveForward(self,pos):
-	self.position(pos)
-
-
-    def moveBackward(self,pos,speed):
-	self.position(-pos)
-
-    def moveBackward(self,pos):
-	self.position(-pos)
-
-
-    def rotation(self,angle,speed):
-        speed = speedToCommand(speed)
-        self.disable()
-        self.board.sendCommand(Commands.ROTATE,speed,angle)
-        self.enable()
-
-    def rotation(self,angle):
-        rotation(angle,DEFAULT_SPEED)
-
-
-    def turnLeft(self,angle,speed):
-	self.rotation(angle,speed)
-
-    def turnLeft(self,angle):
-	self.rotation(angle)
-
-
-    def turnRight(self,angle,speed):
-	self.rotation(-angle,speed)
-
-    def turnRight(self,angle):
-	self.rotation(-angle)
+    def turnRight(self,angle,speed=DEFAULT_SPEED, block=True):
+        self.rotate(speed, angle)
 
     def __del__(self):
         self.stop()
-
-#sleep(1)
-#print board.sendCommand(Commands.READ_ULTRASOUND,17,19)
-'''
