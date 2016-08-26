f2d9a28fee5d54e32d6337e094647349ead61e0c
Harry Gee <code@pibot.org>
2016-08-26 11:25:01 +0100

    Update NewMattTest.py

--- tests/NewMattTest.py
+++ tests/NewMattTest.py
@@ -10,7 +10,7 @@ mission.startMission()
 
 # each command will wait (block) until the robot has finished moving
 # speed is the default speed
-move.moveForward(20) # move forwards 100cm
-move.moveBackward(20) # move backwards 100cm
-#move.turnLeft(90) # turn left 90 degrees
-#move.turnRight(90) # turn right 90 degrees
+move.moveForward(20) # move forwards 20cm
+move.moveBackward(20) # move backwards 20cm
+move.turnLeft(90) # turn left 90 degrees
+move.turnRight(90) # turn right 90 degrees
